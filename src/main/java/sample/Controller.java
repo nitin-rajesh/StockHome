9a77396cdbecd045a7b289ee3380b77a1d461565
IT Guy <nitinkrajesh@gmail.com>
2021-12-12 14:01:36 +0530

    Initial commit

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    ComboBox<String> UserNameBox;
+
+    @FXML
+    TextField PasswordBox;
+
+    @FXML
+    void loginButton(ActionEvent e){
+
+    }
+
+    @FXML
+    void registerButton(ActionEvent e){
+
+    }
+}
